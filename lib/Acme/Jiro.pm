b4fa8bdf7c3003fc80ab2bd9855a61707185a75e
Syohei YOSHIDA <syohex@gmail.com>
2011-11-20 10:36:55 +0900

    init repos

---
+++ lib/Acme/Jiro.pm
@@ -0,0 +1,41 @@
+package Acme::Jiro;
+
+use 5.008_001;
+use strict;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Acme::Jiro -
+
+=head1 SYNOPSIS
+
+  use Acme::Jiro;
+
+=head1 DESCRIPTION
+
+Acme::Jiro is
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011- Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
